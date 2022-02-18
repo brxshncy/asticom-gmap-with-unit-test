8bdc8e03d56270be4c1e82cbc469abe461994b44
bruce <ggkidsbruce619@gmail.com>
2022-02-19 07:51:43 +0800

    init

---
+++ src/components/Layout/MainLayout.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Layout, Menu } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+
+interface MainLayoutProps {
+  children: React.ReactChild;
+}
+
+const { Sider, Content } = Layout;
+export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <Layout>
+      {" "}
+      <Sider>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+        </Menu>{" "}
+      </Sider>{" "}
+      <Layout>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
